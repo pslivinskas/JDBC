a357ded10db681daff679ffd5f087b2836fb3cc2
pslivinskas <povilaslsivinskas@gmail.com>
2023-01-13 13:51:32 +0200

    Added attributes to Customer.java

--- src/main/java/JDBC/Customer.java
+++ src/main/java/JDBC/Customer.java
@@ -3,8 +3,12 @@ package JDBC;
 public class Customer {
     private int customerNumber;
     private String customerName;
+    private String contactLastName;
+    private String contactFirstName;
     private String phone;
+    private String addressLine1;
     private String city;
+    private String country;
 
     public Customer(int customerNumber, String customerName) {
         this.customerNumber = customerNumber;
@@ -16,6 +20,24 @@ public class Customer {
         this.customerName = customerName;
         this.phone = phone;
         this.city = city;
+    }
+
+    public Customer(int customerNumber,
+                    String customerName,
+                    String contactLastName,
+                    String contactFirstName,
+                    String phone,
+                    String addressLine1,
+                    String city,
+                    String country) {
+        this.customerNumber = customerNumber;
+        this.customerName = customerName;
+        this.contactLastName = contactLastName;
+        this.contactFirstName = contactFirstName;
+        this.phone = phone;
+        this.addressLine1 = addressLine1;
+        this.city = city;
+        this.country = country;
     }
 
     public int getCustomerNumber() {
@@ -30,16 +52,56 @@ public class Customer {
         return phone;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
     public String getCity() {
         return city;
     }
 
+    public String getContactLastName() {
+        return contactLastName;
+    }
+
+    public String getContactFirstName() {
+        return contactFirstName;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setContactLastName(String contactLastName) {
+        this.contactLastName = contactLastName;
+    }
+
+    public void setContactFirstName(String contactFirstName) {
+        this.contactFirstName = contactFirstName;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
     public void setCity(String city) {
         this.city = city;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     @Override
